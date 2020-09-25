3827945bf1a5770a753f7ebf37438843a41ab430
hiro7778 <stfhm10969@gmail.com>
2020-09-26 02:38:38 +0900

    モデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  validates :nickname,             presence: true
+  #validates :email,                presence: true
+  validates :birth_date,           presence: true 
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,84 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
   describe 'ユーザー新規登録' do
-    it "ニックネームが必須であること" do
-      user = User.new(nickname:"")
+    context 'ユーザー情報' do
+      it "ニックネームがないと登録できない" do
+        @user.nickname = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it 'メールアドレスがないと登録できない' do
+        @user.email = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it 'メールアドレスが一意性でないと登録できない' do
+        @user.save
+        @another_user = FactoryBot.build(:user, email: @user.email)
+        @another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
+      it 'メールアドレスは@を含む必要があること' do
+        @user.email = "gmail.com"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
+      it 'パスワードがないと登録できない' do
+        @user.password = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'パスワードは6文字未満は登録できない' do
+        @user.password = "aaaaa"
+        @user.password_confirmation = "aaaaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+      it 'パスワードは半角英数字混合でないと登録できない' do
+        @user.password = "bbbbbb"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
+      end
+      it 'パスワードは確認用を含めて2回入力すること' do
+        @user = FactoryBot.build(:user, password_confirmation: "")
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
     end
+
+    context '本人情報確認' do
+      it 'ユーザー本名が、名字と名前がそれぞれないと登録できない' do
+        @user.last_name = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name can't be blank")
+      end
+      it 'ユーザー本名は全角（漢字・ひらがな・カタカナ）でないと登録できない' do
+        @user.last_name = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name 全角文字を使用してください")
+      end
+      it 'ユーザー本名のフリガナが、名字と名前でそれぞれないと登録できない' do
+        @user.last_name_kana = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana can't be blank")
+      end
+      it 'ユーザー本名のフリガナは全角（カタカナ）で入力させること' do
+        @user.last_name_kana = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana 全角文字を使用してください")
+      end
+      it '生年月日がないと登録できない' do
+        @user.birth_date = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birth date can't be blank")
+      end
+    end
+
   end
 end
