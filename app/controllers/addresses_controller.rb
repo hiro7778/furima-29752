1150cdc7dfd8d400b2b052997eb79d92d7878dc8
hiro7778 <stfhm10969@gmail.com>
2020-10-05 19:25:00 +0900

    出品者がURL直打ちした場合トップページへ
    出品者はURLを直接入力して購入ページに遷移しようとすると、トップページに遷移する

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,5 +1,14 @@
 class AddressesController < ApplicationController
   def index
-    
+    @item = Item.find(params[:item_id])
+
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
+
+    unless user_signed_in?
+      redirect_to root_path
+    end
+
   end
 end
