fd27ba6da92ef63f541fceb6c9f63859dd41bcd9
hiro7778 <stfhm10969@gmail.com>
2020-10-06 01:52:46 +0900

    purchasesコントローラーの記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,7 @@
 class PurchasesController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @purchase = Address.new
 
     if current_user.id == @item.user_id
       redirect_to root_path
@@ -11,4 +12,31 @@ class PurchasesController < ApplicationController
     end
   end
 
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase = Address.new(purchase_params)
+
+    if @purchase.valid?
+      pay_item
+      @purchase.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:address).premit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency:'jpy'
+    )
+  end
 end
