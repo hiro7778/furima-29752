31aa088e7cc82f3dc0c1143dccf9a2768e40d397
hiro7778 <stfhm10969@gmail.com>
2020-10-07 01:01:05 +0900

    モデルのテストコード実装

--- app/forms/purchase_address.rb
+++ app/forms/purchase_address.rb
@@ -3,15 +3,15 @@ class PurchaseAddress
   attr_accessor :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :token, :item_id, :user_id
 
   with_options presence: true do
-    validates :post_code,    format: { with: /\A[0-9]+\z/, message: "半角数字のみで入力して下さい"}
+    validates :post_code,    format: { with: /\A\d{3}[-]\d{4}\z/, message: "半角数字とハイフンで入力して下さい"}
     validates :city,         format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "全角文字を使用してください"}
 
-    validates :phone_number, format: { with: /\A[0-9]+\z/, message: "半角数字のみで入力して下さい"}
+    validates :phone_number, format: { with: /\A\d{11}\z/, message: "半角数字11桁で入力して下さい"}
   end
 
   validates :token, presence: true
   validates :house_number, presence: true
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank"}
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
--- spec/factories/purchase_address.rb
+++ spec/factories/purchase_address.rb
@@ -3,8 +3,8 @@ FactoryBot.define do
     post_code { '123-4567' }
     prefecture_id {2}
     city { '東京都' }
-    house_number { '111' }
-    building_name { '渋谷222' }
+    house_number { '渋谷111' }
     phone_number { '09011112222' }
+    token { 'tok_abcdefghijk00000000000000000' }
   end
 end
