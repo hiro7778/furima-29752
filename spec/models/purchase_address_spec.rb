be957ca3b0bb3bdcd810eec512e9378be7a80c10
hiro7778 <stfhm10969@gmail.com>
2020-10-07 12:59:08 +0900

    修正

--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -58,6 +58,11 @@ RSpec.describe PurchaseAddress, type: :model do
         @purchase_address.valid?
         expect(@purchase_address.errors.full_messages).to include("Token can't be blank")
       end
+      it '電話番号が11桁未満のとき' do
+        @purchase_address.phone_number = '09012345678'
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Phone number 半角数字11桁で入力して下さい")
+      end
     end
 
   end
